f451dac5605656063efca4a2bb308dfe2c824d56
BootWP <burdianov@gmail.com>
2022-12-28 12:43:46 +0200

    Add 404 page template

---
+++ 404.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * 404 template file
+ *
+ */
+get_header();?>
+
+<div class="ll-page-content container">
+	<section class="ll-content">
+		<div class="ll-page-header container">
+			<span class="ll-supheader">Лейка</span>
+			<h1 class="ll-header">404</h1>
+		</div>
+
+		<div class="ll-page-content container">
+			<section class="ll-content">
+				<article class="page type-page hentry">
+					<div class="post-content">
+						<p>Такой странице на сайте не существует.</p>
+					</div>
+				</article>
+			</section>
+		</div>
+	</section>
+</div>
+
+<?php
+get_footer();
